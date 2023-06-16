f2d329632cff14e204f46b5d32e99b00e0830250
managerof <109286391+managerof@users.noreply.github.com>
2023-06-16 15:47:31 +0100

    Revert "Revert "begin vasya""
    This reverts commit 7d83a7944a3dca3d123b4653bc4cdcfaed1d173c.

---
+++ main.py
@@ -0,0 +1,25 @@
+import telebot
+import db as utils
+
+bot = telebot.TeleBot('6048696984:AAFLoKqEys0oloCNlXWCdB_1UHqS8oAglEo')
+
+db = utils.Database(utils.USERS_DATA_FILE_PATH)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    
+    user = utils.User(message.from_user.id,
+                      message.from_user.first_name,
+                      message.date,
+                      message.from_user.language_code)
+    
+#     if db.IsNewbie(user.id):
+#         db.addUser(user)
+    
+    bot.send_message(user.id, f"{user.name}, ваш індетефіційний телеграм номер - {user.id}")
+    
+    bot.reply_to(message, "Привіт! Я бот від Lifecell і допоможу вам підібрати найкращий тариф!")
+
+bot.polling()
+
